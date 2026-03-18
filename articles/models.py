d3337e963546b6c8ae7a86643c05de4ec6bd30f1
Tatiana-Juin <username@users.noreply.github.com>
2026-03-18 12:13:51 +0100

    Je viens de créer mon models article

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Articles(models.Model):
+    titre_article = models.CharField()
+    titre_1 = models.CharField(max_length=300)
+    paragraphe_1 = models.TextField()
+    titre_2 = models.CharField(max_length=300,blank=True, null=True)
+    paragraphe_2 = models.TextField(blank=True, null=True)
+    titre_3 = models.CharField(max_length=300,blank=True, null=True)
+    paragraphe_3 = models.TextField(blank=True, null=True)
+    titre_4 = models.CharField(max_length=300,blank=True, null=True)
+    paragraphe_4 = models.TextField(blank=True, null=True)
+    
+    def __str__(self):
+        return f"id : {self.id} titre : {self.titre_article}"
+    
